4604cbedc49bd3d58673cbde9e2eb3dfda4afcb9
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-02-10 23:56:17 +0100

    Changed chevrons in footer so that it looks good.

--- script.js
+++ script.js
@@ -32,11 +32,23 @@ console.log(linkAbout);
 let linkAide = document.querySelector('#links-aide');
 console.log(linkAide);
 
+let chevronMinus = document.querySelector('#chevronMinus');
+console.log(chevronMinus);
+
+let chevronAbout = document.querySelector('#chevronAbout');
+console.log(chevronAbout);
+
+let chevronAide = document.querySelector('#chevronAide');
+console.log(chevronAide);
+
+
 navMinus.addEventListener('click', function(){
     console.log('entered event');
     if (linkMinus.classList.contains('nav-footer-off')){
+        chevronMinus.setAttribute('src', 'img/chevron-down.svg')
         linkMinus.classList.replace('nav-footer-off', 'nav-footer-on'); 
     } else {
+        chevronMinus.setAttribute('src', 'img/chevron-right.svg')
         linkMinus.classList.replace('nav-footer-on', 'nav-footer-off');
     }
 });
@@ -44,8 +56,10 @@ navMinus.addEventListener('click', function(){
 navAbout.addEventListener('click', function(){
     console.log('entered event');
     if (linkAbout.classList.contains('nav-footer-off')){
+        chevronAbout.setAttribute('src', 'img/chevron-down.svg')
         linkAbout.classList.replace('nav-footer-off', 'nav-footer-on'); 
     } else {
+        chevronAbout.setAttribute('src', 'img/chevron-right.svg')
         linkAbout.classList.replace('nav-footer-on', 'nav-footer-off');
     }
 });
@@ -53,8 +67,10 @@ navAbout.addEventListener('click', function(){
 navAide.addEventListener('click', function(){
     console.log('entered event');
     if (linkAide.classList.contains('nav-footer-off')){
+        chevronAide.setAttribute('src', 'img/chevron-down.svg')
         linkAide.classList.replace('nav-footer-off', 'nav-footer-on'); 
     } else {
+        chevronAide.setAttribute('src', 'img/chevron-right.svg')
         linkAide.classList.replace('nav-footer-on', 'nav-footer-off');
     }
 });
